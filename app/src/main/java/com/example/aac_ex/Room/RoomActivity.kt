adb0b29d6b858b471208d0d23c8dee4c12c02a1c
Gihyeon-Kwon <rlg1133@naver.com>
2020-09-28 16:41:45 +0900

    비동기 처리

--- app/src/main/java/com/example/aac_ex/Room/RoomActivity.kt
+++ app/src/main/java/com/example/aac_ex/Room/RoomActivity.kt
@@ -3,17 +3,20 @@ package com.example.aac_ex.Room
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.aac_ex.R
 import kotlinx.android.synthetic.main.activity_room.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class RoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
 
-        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-name").allowMainThreadQueries().build()
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "database-name").build()
         //allowMainThreadQueries은 메인스레드에서도 룸디비를 사용할 수 있게 해주는것, 실전에서는 잘 안씀
 
         //it은 todolist
@@ -23,7 +26,12 @@ class RoomActivity : AppCompatActivity() {
 
         //데이터 업뎃되면 알아서 적용되니 리스너에서 추가 안해줘도 됨
         btn_add.setOnClickListener{
-            db.todoDao().insert(Todo(et_todo.text.toString()))
+            //Dispatchers.IO 를 통해 백그라운드에서 돌아가게
+            //lifecyleScope로 코루틴을 사용한 비동기처리 해줌
+            lifecycleScope.launch(Dispatchers.IO) {
+                db.todoDao().insert(Todo(et_todo.text.toString()))
+            }
+
         }
     }
 }
