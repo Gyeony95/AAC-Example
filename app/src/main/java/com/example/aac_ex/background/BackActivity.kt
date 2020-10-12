8ea29a2263fdf4e96cbc838d4678beb82d006775
Gihyeon-Kwon <rlg1133@naver.com>
2020-10-12 23:03:05 +0900

    modify

---
+++ app/src/main/java/com/example/aac_ex/background/BackActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aac_ex.background
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aac_ex.R
+
+class BackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_back)
+    }
+}
